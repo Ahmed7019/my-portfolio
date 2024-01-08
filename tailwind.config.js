9d280c6e04ac408b9b2bc19c1d2472b7dd75cece
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-01-08 17:13:19 +0200

    modified configuration settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html"],
+  content: [
+    "index.html",
+    "./dist/*.html"],
   theme: {
     extend: {
       fontFamily: {
