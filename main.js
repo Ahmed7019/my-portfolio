e6e781cf8976b1bf7fe9bfcffbd858f759e5710a
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-03-07 10:37:54 +0200

    added styles to menu

---
+++ main.js
@@ -0,0 +1,5 @@
+// declare variables of menu bar buttons
+
+let menuBtn = document.querySelector("#menu-btn"),
+  closeBtn = document.querySelector("#close-btn"),
+  menu = document.querySelector("#menu");
