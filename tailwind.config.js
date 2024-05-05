e70b0756dfca49c78ea3b9ee29cb0bb3ab5936a9
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-05-05 07:26:55 +0200

    added animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["index.html", "./dist/*.html"],
   theme: {
     extend: {
+      keyframes: {
+        "border-spin": {
+          "100%": {
+            transofrm: "rotate(-360deg)",
+          },
+        },
+      },
+      animation: {
+        "border-spin": "border-spin 7s linear infinite",
+      },
       fontFamily: {
         "play-fair": ["Playfair Display", "serif"],
       },
