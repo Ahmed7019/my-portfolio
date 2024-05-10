f30eb422a510cb1da83880dce89da72258f00b0d
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-05-10 14:33:29 +0200

    added sweet alert and did the js configurations

--- contact.js
+++ contact.js
@@ -2,3 +2,22 @@ let contactForm = document.querySelector("#contact-form");
 contactForm.addEventListener("submit", (e) => {
   e.preventDefault();
 });
+
+// Send email
+
+let sendMail = () => {
+  let info = {
+    from_name: document.querySelector("#name").value,
+    email_id: document.querySelector("#email").value,
+    message: document.querySelector("#msg").value,
+  };
+  emailjs.send("service_d2eedio","template_plffa2i",info).then(function (res) {
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Your message is sent successfully!",
+        showConfirmButton: false,
+        timer: 1500
+      });
+  } )
+};
