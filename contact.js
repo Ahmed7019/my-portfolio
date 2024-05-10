ff2c8ae23d4e6879fa2c80df4cb542abdd76c2dc
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-05-10 15:46:34 +0200

    all done :)

--- contact.js
+++ contact.js
@@ -1,23 +1,3 @@
-let contactForm = document.querySelector("#contact-form");
-contactForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
+// Import sweet alert
 
-// Send email
 
-let sendMail = () => {
-  let info = {
-    from_name: document.querySelector("#name").value,
-    email_id: document.querySelector("#email").value,
-    message: document.querySelector("#msg").value,
-  };
-  emailjs.send("service_d2eedio","template_plffa2i",info).then(function (res) {
-    Swal.fire({
-        position: "top-end",
-        icon: "success",
-        title: "Your message is sent successfully!",
-        showConfirmButton: false,
-        timer: 1500
-      });
-  } )
-};
